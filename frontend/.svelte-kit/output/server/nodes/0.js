80e28044638495dccfa995e06a0ff75b4e7f6b08
mcowin <vinvladik@gmail.com>
2025-08-16 05:18:35 +0300

    fix

--- frontend/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.BIhlmrq3.js","_app/immutable/chunks/DsnmJJEf.js","_app/immutable/chunks/C_4fjHm8.js","_app/immutable/chunks/YmlnBEzx.js","_app/immutable/chunks/tiTolGyO.js"];
-export const stylesheets = ["_app/immutable/assets/0.CxUGFkA0.css"];
-export const fonts = [];
